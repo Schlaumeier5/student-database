79a3723f8fec6a93bd2190b61ddb6e6dde023c42
Lukas Morgenstern <lukas.morgenstern@icloud.com>
2025-08-08 17:10:55 +0200

    Further improved command execution and made sure commands are being executed

--- src/main/java/de/igslandstuhl/database/server/commands/Command.java
+++ src/main/java/de/igslandstuhl/database/server/commands/Command.java
@@ -1,6 +1,8 @@
 package de.igslandstuhl.database.server.commands;
 
 import de.igslandstuhl.database.Registry;
+import de.igslandstuhl.database.api.Admin;
+import de.igslandstuhl.database.utils.CommonUtils;
 
 @FunctionalInterface
 public interface Command {
@@ -12,5 +14,22 @@ public interface Command {
         } catch (NullPointerException e) {
             return "Command not found: " + command;
         }
+    }
+    public static void registerCommand(String name, Command command) {
+        Registry.commandRegistry().register(name, command);
+    }
+    public static void registerCommands() {
+        // Add admin
+        registerCommand("add-admin", (args) -> {
+            if (args.length < 2) return "Usage: add-admin [username] [password]";
+            try {
+                String username = args[0];
+                String password = args[1];
+                Admin.create(username, password);
+            } catch (Exception e) {
+                return "Error while trying to add admin:\n" + CommonUtils.getStacktrace(e);
+            }
+            return "Successfully added admin";
+        });
     }
 }
--- src/main/java/de/igslandstuhl/database/utils/CommandLineUtils.java
+++ src/main/java/de/igslandstuhl/database/utils/CommandLineUtils.java
@@ -3,11 +3,18 @@ package de.igslandstuhl.database.utils;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import de.igslandstuhl.database.server.commands.Command;
+
 public class CommandLineUtils {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static String input(Object... messages) {
         Arrays.stream(messages).forEach((m) -> System.out.print(m.toString() + " "));
         System.out.print(">>>");
         return SCANNER.nextLine();
+    }
+    public static void waitForCommandAndExec() {
+        String[] args = input().split(" ");
+        String command = args[0];
+        System.out.println(Command.executeCommand(command, Arrays.copyOfRange(args, 1, args.length)));
     }
 }
