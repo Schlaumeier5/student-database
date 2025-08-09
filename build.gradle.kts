c10c5e7221126980d9aba30abd039416ae585268
Lukas Morgenstern <lukas.morgenstern@icloud.com>
2025-08-09 22:48:18 +0200

    Changed build manager to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    application
+}
+
+group = "igs-landstuhl"
+version = "1.0-SNAPSHOT"
+application {
+    mainClass.set("de.igslandstuhl.database.server.Server") // update to your actual main class if different
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.xerial:sqlite-jdbc:3.50.3.0")
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("commons-codec:commons-codec:1.19.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0") // using JUnit 5 (latest)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17)) // or another version you prefer
+    }
+}
