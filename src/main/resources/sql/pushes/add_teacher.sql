21193045eb7e3920e93edc9a79cc220f92678560
Lukas Morgenstern <lukas.morgenstern@icloud.com>
2025-08-17 13:49:15 +0200

    fixed #120

--- src/main/resources/sql/pushes/add_teacher.sql
+++ src/main/resources/sql/pushes/add_teacher.sql
@@ -2,4 +2,6 @@ INSERT INTO teachers (first_name, last_name, email, password)
 VALUES (?, ?, ?, ?)
 ON CONFLICT(first_name, last_name) DO UPDATE SET
     email = excluded.email,
+    password = excluded.password
+ON CONFLICT(email) DO UPDATE SET
     password = excluded.password;
