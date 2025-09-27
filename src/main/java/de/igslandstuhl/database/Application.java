2de64948155cd382039ec53937515a23d4373361
Lukas Morgenstern <lukas.morgenstern@icloud.com>
2025-09-27 13:56:11 +0200

    Command line is now initialized before server This is to ensure it is loaded before the user is asked for database path and keystore path and password

--- src/main/java/de/igslandstuhl/database/Application.java
+++ src/main/java/de/igslandstuhl/database/Application.java
@@ -92,6 +92,12 @@ public final class Application {
 
     public static void main(String[] args) throws Exception {
         instance = new Application(args);
+        
+        if (!getInstance().suppressCmd()) {
+            Command.registerCommands();
+            CommandLineUtils.setup();
+        }
+        
         Server.getInstance().getConnection().createTables();
 
         Holiday.setupCurrentSchoolYear();
@@ -99,10 +105,6 @@ public final class Application {
 
         if (getInstance().runsWebServer()) {
             Server.getInstance().getWebServer().start();
-        }
-        if (!getInstance().suppressCmd()) {
-            Command.registerCommands();
-            CommandLineUtils.setup();
         }
 
         while (true) {
