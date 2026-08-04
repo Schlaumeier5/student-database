7f5784611673af987759e7070888539cac0922ff
synchronierer <153615360+synchronierer@users.noreply.github.com>
2026-08-04 22:30:44 +0200

    Allow configuring the web server port for runtime

--- src/main/java/de/igslandstuhl/database/server/Server.java
+++ src/main/java/de/igslandstuhl/database/server/Server.java
@@ -94,7 +94,9 @@ public final class Server implements AutoCloseable {
             String keystorePath = Application.getInstance().runsWebServer() ? Application.getInstance().getOptionSafe("keystore", "keys/web/keystore.jks") : null;
             String keystorePassword = Application.getInstance().runsWebServer() ? Application.getInstance().getOptionSafe("keystore-password", "changeit") : null;
             String keystoreType = Application.getInstance().runsWebServer() ? Application.getInstance().getOptionSafe("keystore-type", "JKS") : null;
-            int port = 443;
+            int port = Integer.parseInt(
+                Application.getInstance().getOptionSafe("port", "443")
+            );
             webServer = Application.getInstance().runsWebServer() ? new WebServer(port, keystorePath, keystorePassword, keystoreType) : new WebServer();
         } catch (Exception e) {
             throw new IllegalStateException("Server failed on start", e);
